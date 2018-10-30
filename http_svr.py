26946025825481c5c9865d99a255c211a27f2838
Vic Chimenti <chimenti@seattleu.edu>
2018-10-30 09:26:40 -0700

    Update http_svr.py
    reverse message protocol added

--- http_svr.py
+++ http_svr.py
@@ -15,22 +15,35 @@ import sys
 
 
 
+# Function to reverse a string
+def reverse(string):
+    string = string[::-1]
+    return string
+
+
+
+
 HOST = '127.0.0.1'          # Default localhost
 PORT = 10100                # Default Port - Assigned Range is 10100 - 10109
 user_input = sys.argv[1]    # User Defined Port Number
 
 
 # open socket connection for TCP stream and listen
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    client, address = sock.accept()
 
     # echo response
-    with conn:
-        print('Connected by', addr)
+    with client:
+        print('Connection Established With: ', address)
         while True:
-            data = conn.recv(1024)
-            if not data:
+            message = client.recv(65536)
+            if not message:
                 break
-            conn.sendall(data)
+        response = (reverse(message))
+        client.sendall(response)
+
+
+client.close()              # Close the Client Socket
+sys.exit()                  # Exit the Program
