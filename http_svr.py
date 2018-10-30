f352070a6310a01b8d60519a274514dafc682b9d
Vic Chimenti <chimenti@seattleu.edu>
2018-10-30 09:43:31 -0700

    Update http_svr.py

--- http_svr.py
+++ http_svr.py
@@ -22,11 +22,11 @@ def reverse(string):
 
 # Function to display hostname and
 # IP address
-def get_Host_name_IP():
+def get_host_name_IP():
     try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        print("Hostname :  ",host_name)
+        host = socket.gethostname()
+        host_ip = socket.gethostbyname(host)
+        print("Hostname :  ",host)
         print("IP : ",host_ip)
     except:
         print("Unable to get Hostname and IP")
@@ -36,19 +36,21 @@ def get_Host_name_IP():
 
 
 
-HOST = '127.0.0.1'          # Default localhost
-PORT = 10100                # Default Port - Assigned Range is 10100 - 10109
-get_Host_name_IP()          # Identify Domain and IP on current machine 
+host_ip = '127.0.0.1'       # Default localhost IP address
+port = 10100                # Default Port - Assigned Range is 10100 - 10109
+get_host_name_IP()          # Identify Domain and IP on current machine
 user_input = sys.argv[1]    # User Defined Port Number
+
+
 
 
 # open socket connection for TCP stream and listen
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
+    sock.bind((host_ip, port))
     sock.listen()
     client, address = sock.accept()
 
-    # echo response
+    # reverse response
     with client:
         print('Connection Established With: ', address)
         while True:
@@ -57,6 +59,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 break
         response = (reverse(message))
         client.sendall(response)
+
+
 
 
 client.close()              # Close the Client Socket
