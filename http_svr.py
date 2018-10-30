b12a545e96cba911994258283f1ff35ad925ffb2
Vic Chimenti <chimenti@seattleu.edu>
2018-10-30 09:11:53 -0700

    Update http_svr.py
    set defaults and class assignment header

--- http_svr.py
+++ http_svr.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python3
+# Vic Chimenti
+# CPSC 5510 p1b
+# http_svr.py
+# A Simple Web Server in Python3
+# Created           10/30/2018
+# Last Modified     10/30/2018
+# /usr/bin/python3
 
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = '127.0.0.1'  # Default localhost
+PORT = 10100        # Default Port - Assigned Range is 10100 - 10109
 
+
+# open socket connection for TCP stream and listen
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+
+    # echo response
     with conn:
         print('Connected by', addr)
         while True:
